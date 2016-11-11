34c5b0e11c2de4b720c4c11af9d63aa3a343a5af
kanth989 <kanth989@gmail.com>
2016-11-11 18:03:52 +0530

    Added logic for Rook and Queen

--- chess_exer.py
+++ chess_exer.py
@@ -33,8 +33,56 @@ class Board(object):
     def corner_blocks(self):
         all = self.all_blocks()
         return list(set(all) - set(self.edge_blocks()))
+
+    def veritical_dir(self):
+        return [self.pos_alfa +str(x) for x in self.Y]
+
+    def horizontal_dir(self):
+        return [x + str(self.pos_num) for x in self.X]
     
-    
+    def corner_dir(self):
+        # horz = self.horizontal_dir()
+        # hor = [horz[0], horz[-1]]
+        leng = len(self.X[self.X.index(self.pos_alfa):])
+        # k = [x+str(y)  for x in  self.X[self.pos_num -1 :] for y in self.Y[self.pos_num:]]
+        # print leng
+        # right up
+        right_total_up = leng + self.pos_num
+        # print right_total_up , "ioooo"
+        if (8 -right_total_up) < 0:
+            right_up_coord = self.X[8-right_total_up]+str(8)
+        else:
+            right_up_coord = 'h' + str(right_total_up-1) 
+        # right down
+        right_total_down = leng - self.pos_num
+        # print right_total_down
+
+        if right_total_down >= 0 :
+            right_down_coord = self.X[-right_total_down-1] + str(1)
+        else: 
+            right_down_coord = 'h' + str(self.Y[-right_total_down])
+
+
+        # left_up
+        
+        left_up_total = 8 - leng  + self.pos_num
+        # print left_up_total
+        if left_up_total - 8  >= 0 :
+            left_up_coord =  self.X[left_up_total - 8  ] + str(8)
+        else: 
+            left_up_coord = 'a' + str(left_up_total)
+
+        left_down_total = (8 - leng) - self.pos_num
+        
+        # left down
+        if left_down_total < 0:
+            left_down_coord = 'a' + str(self.Y[ - left_down_total -1])
+        else:
+            left_down_coord = self.X[left_down_total  + 1] + str(1)
+        # print left_down_coord
+
+        return [right_up_coord, right_down_coord, left_up_coord, left_down_coord]
+
 def filter_results(result):
     X = ['a','b','c','d','e','f','g','h']
     Y = [1,2,3,4,5,6,7,8]
@@ -53,14 +101,20 @@ if __name__ == "__main__":
     name = sys.argv[1]
     pos = sys.argv[2]
     print name , pos
+    print "*-----------------*"
     
     try:
         given = Board(pos)
-        # print given.edge_blocks()
-        # print given.corner_blocks()
         if name.startswith('k'):
-            print knight_possibilities(given.corner_blocks(), given.all_blocks())
-        
+            print "Jumps to", knight_possibilities(given.corner_blocks(), given.all_blocks())
+        if name.startswith('r'): 
+            print "Vertical", given.veritical_dir()[0], given.veritical_dir()[-1]
+            print "Horizontal", given.horizontal_dir()[0], given.horizontal_dir()[-1]
+        if name.startswith('q'):
+            print "Vertical",  given.veritical_dir()[0], given.veritical_dir()[-1]
+            print "Horizontal",given.horizontal_dir()[0], given.horizontal_dir()[-1]
+            print "Corners", given.corner_dir()
+            
     except Exception, e:
         print e
         print "Something went wrong with your input."
